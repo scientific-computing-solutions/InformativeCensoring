68e85566040348635346ec67f7d132a16606c643
Ruau, David J <David.Ruau@astrazeneca.com>
2016-07-26 12:04:51 +0100

    Fising namespace complains

---
+++ R/InformativeCensoring-package.R
@@ -0,0 +1,8 @@
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib InformativeCensoring
+#' @importFrom survival Surv
+#' @import parallel
+#' @importFrom stats formula model.frame na.fail pf predict pt 
+#' @importFrom stats qt quantile runif setNames terms update var vcov
+#' @importFrom utils tail
+NULL
