455bbfeda33f8e22a8c96f8747f0dc8e7009ad25
Ruau, David J <David.Ruau@astrazeneca.com>
2017-02-08 15:55:15 +0000

    Corrected email address typo

--- R/dataDescription.R
+++ R/dataDescription.R
@@ -4,29 +4,29 @@
 
 ##' Perform methods of multiple imputation for
 ##' time to event data
-##' 
+##'
 ##' See Nonparametric comparison of two survival functions with
 ##' dependent censoring via nonparametric multiple imputation. Hsu and Taylor
 ##' Statistics in Medicine (2009) 28:462-475 for Hsu's method
-##' 
+##'
 ##' See Relaxing the independent censoring assumption in the Cox proportional
 ##' hazards model using multiple imputation. Jackson et al., Statistics in Medicine
-##' (2014) 33:4681-4694 for Jackson's method 
+##' (2014) 33:4681-4694 for Jackson's method
 ##'
 ##' @name InformativeCensoring-package
 ##' @aliases InformativeCensoring
 ##' @docType package
 ##' @title Perform methods of multiple imputation for
 ##' time to event data
-##' @author \email{David.Ruaa@@astrazeneca.com}
+##' @author \email{David.Ruau@@astrazeneca.com}
 NULL
 
 
 ##' Simulated time to event data with 5 time independent covariates
-##' 
+##'
 ##' This dataset is inspired by the simulation described in Hsu and Taylor,
 ##' Statistics in Medicine (2009) 28:462-475 with an additional DCO.time column
-##' 
+##'
 ##' @name ScoreInd
 ##' @docType data
 ##' @format A data.frame containing a row per subject with eleven columns:
@@ -46,18 +46,18 @@ NULL
 NULL
 
 ##' Simulated time dependent variables for time to event data
-##' 
+##'
 ##' This data set contains time dependent covariates for the
-##' \code{\link{ScoreInd}} time to event data. 
-##' 
-##' @name ScoreTimeDep 
+##' \code{\link{ScoreInd}} time to event data.
+##'
+##' @name ScoreTimeDep
 ##' @docType data
 ##' @format A data.frame containing 1 row per subject-visit
 ##' @field Id The Subject Id
 ##' @field start The covariate given in each row are for a given subject from time 'start'...
 ##' @field end ... until time end
 ##' @field W1 The value of a (binary) time dependent variable
-##' for the subject with the given 'Id' between times 'start' and 'end' 
+##' for the subject with the given 'Id' between times 'start' and 'end'
 ##' @field W2 The value of a (continuous) time dependent variable
 ##' for the subject with the given 'Id' between times 'start' and 'end'
 NULL
